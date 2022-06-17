aa7e2f499c5691882954ad0d04a3f9ab4eea4e90
FrankLef <flefebvre01@hotmail.com>
2022-06-17 03:45:12 -0400

    fence

--- R/prune_02-B_fence.R
+++ R/prune_02-B_fence.R
@@ -18,7 +18,7 @@ prune_fence <- function(data, cols, is_offset = TRUE) {
   # compute x and y with offset
   x <- data[, cols[1]] - the_offset
   y <- data[, cols[2]] - the_offset
-  # all values must be >= 0
+  # all values must be >= 0 and finite
   checkmate::assertNumeric(x, finite = TRUE, lower = 0)
   checkmate::assertNumeric(y, finite = TRUE, lower = 0)
 
@@ -29,64 +29,53 @@ prune_fence <- function(data, cols, is_offset = TRUE) {
 }
 
 
-prune_fence_slopes <- function(x, y) {
+prune_fence_slopes <- function(x, y, tol = .Machine$double.eps^0.5) {
   checkmate::assertNumeric(x, finite = TRUE, any.missing = FALSE, min.len = 3)
   checkmate::assertNumeric(y, finite = TRUE, any.missing = FALSE, len = length(x))
+  checkmate::assertNumber(tol, lower = 0, finite = TRUE)
 
-  # must only used values > 0 to compute the slope
+  # must only used values positive values to compute the slope
   x_pos <- x[x > 0]
   y_pos <- y[y > 0]
   assertthat::assert_that(length(x_pos) > 0,
-                          msg = "There are no positive values in x.")
+                          msg = "There are no positive values in scaled x.")
   assertthat::assert_that(length(y_pos) > 0,
-                          msg = "There are no positive values in y.")
-  # cat("\n", crayon::bold$yellow(paste("inside:", "x_pos")), "\n")
-  # print(x_pos)
-  # cat("\n", crayon::bold$yellow(paste("inside:", "y_pos")), "\n")
-  # print(y_pos)
+                          msg = "There are no positive values in scaled y.")
+  # cat("\n", "inside: x_pos", "\n")
+  # msg <- sprintf("Range of x_pos: %s", toString(range(x_pos)))
+  # print(msg)
+  # cat("inside: y_pos", "\n")
+  # msg <- sprintf("Range of y_pos: %s", toString(range(y_pos)))
+  # print(msg)
+  # cat("\n")
 
   # the slope used with a given x to determine the limit above which y must be
   small_slope = min(x_pos) / max(y_pos)
   # the slope used with a given x to determine the limit below which y must be
   big_slope = max(x_pos) / min(y_pos)
+  assertthat::assert_that(small_slope > 0, big_slope > 0)
+  # cat("\n", "inside: the slopes", "\n")
+  # print(sprintf("small slope = %f, big slope = %f", small_slope, big_slope))
+  # cat("\n")
 
 
-  if (length(unique(x_pos)) == 1 | length(unique(y_pos)) == 1) {
+  if (abs(diff(range(x_pos))) < tol | abs(diff(range(y_pos))) < tol) {
     # Fencing does not work when at least one of the 2 data vectors
-    # has constantv alues
-    msg_head <- sprintf("All values in x and/or y are the same. Fencing won't work.")
+    # has constantv values
+    msg_head <- "All pos values in x or y are about the same. Fencing won't work."
     msg_head <- cli::col_yellow(msg_head)
     msg_body <- "This happen if at least one of the 2 columns has constant data."
-    msg_body <- c("i" = msg_body,
-                  "i" = sprintf("Mean x value = %f", mean(x_pos)),
-                  "i" = sprintf("Mean y value = %f", mean(y_pos)))
+    msg_body <- c("X" = msg_body,
+                  "i" = sprintf("x pos value range: %s",
+                                toString(range(x_pos))),
+                  "i" = sprintf("y pos value range: %s",
+                                toString(range(y_pos))))
     msg <- paste(msg_head, rlang::format_error_bullets(msg_body), sep = "\n")
     rlang::abort(
       message = msg,
       class = "prune_fence_slopes_error1")
 
-   } else if (small_slope >= big_slope) {
-    msg_head <- sprintf("The low slope must be less than the high slope.")
-    msg_head <- cli::col_yellow(msg_head)
-    msg_body <- c("X" = "Verify the max and min of the SCALED POSITIVE values.",
-                  "i" = sprintf("Small slope = %f", small_slope),
-                  "i" = sprintf("Big slope = %f", big_slope))
-    msg <- paste(msg_head, rlang::format_error_bullets(msg_body), sep = "\n")
-    rlang::abort(
-      message = msg,
-      class = "prune_fence_slopes_error2")
-  } else if (small_slope < .Machine$double.eps^0.5 |
-             big_slope < .Machine$double.eps^0.5) {
-    msg_head <- sprintf("At least one of the 2 slopes is negative.")
-    msg_head <- cli::col_yellow(msg_head)
-    msg_body <- c("X" = "Verify the max and min of the SCALED POSITIVE values.",
-                  "i" = sprintf("Small slope = %f", small_slope),
-                  "i" = sprintf("Big slope = %f", big_slope))
-    msg <- paste(msg_head, rlang::format_error_bullets(msg_body), sep = "\n")
-    rlang::abort(
-      message = msg,
-      class = "prune_fence_slopes_error3")
-  }
+   }
 
   list("small" = small_slope, "big" = big_slope)
 }
--- tests/testthat/test-prune_02-B_fence.R
+++ tests/testthat/test-prune_02-B_fence.R
@@ -95,6 +95,7 @@ test_that("prune_fence_slopes: errors",
 
 
             # Fencing doesn't work when all values are the same
+            # that is, when at least one of the ranges are very small
             x <- rep(1, times = 5)
             y <- rep(2, times = 5)
             expect_error(prune_fence_slopes(x, y),
@@ -108,7 +109,6 @@ test_that("prune_fence_slopes: errors",
                          class = "prune_fence_slopes_error1")
             expect_error(prune_fence_slopes(x, x),
                          class = "prune_fence_slopes_error1")
-
           })
 
 test_that("prune_fence_slopes",
@@ -145,6 +145,21 @@ test_that("prune_fence_slopes",
 
             expect_identical(test_slopes, target_slopes)
 
+            # test when the range of x and range of y are the same
+            x <- c(1 , 2.1, 3.2, 4.3, 5)
+            y <- c(1 , 1.2, 2.3, 3.4, 5)
+
+            x_pos <- x_val[x_val > 0]
+            y_pos <- y_val[y_val > 0]
+            target_small <- min(x_pos) / max(y_pos)
+            target_big <- max(x_pos) / min(y_pos)
+            target_slopes <- list("small" = target_small, "big" = target_big)
+
+
+            test_slopes <- prune_fence_slopes(x_val, y_val)
+
+            expect_identical(test_slopes, target_slopes)
+
 
             # test with data on a wide scale
             x_val <- c(0, 1e4, -1e4, 1e5, 1)
@@ -160,6 +175,4 @@ test_that("prune_fence_slopes",
             test_slopes <- prune_fence_slopes(x_val, y_val)
 
             expect_identical(test_slopes, target_slopes)
-
-
           })
